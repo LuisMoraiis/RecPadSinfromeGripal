bcb592d4373e27632171118d695aed1d5df22866
Luis Morais <fnluismoraiis.750@gmail.com>
2025-11-18 16:41:20 -0300

    Teste dataframe completo

---
+++ analysis/prep_dfc_completo.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.model_selection import train_test_split
+
+import utils.helpers as utils
+
+path = '/home/ubuntu/recPad/RecPadSinfromeGripal/data/raw/RECPAD_SG_COMPLETO.csv'
+
+df = pd.read_csv(path, sep= ',', low_memory= False)
+prefixo_features = 'Sinais e Sintomas'
+
+features = utils.selectFeatures(df, prefixo_features)
+features.append('Resultado do Teste Antigênico')
+
+
+df = df[features]
+df.replace({'Sim': 1, 'Não': 0, 'Ignorado': 2}, inplace= True)
+df['Resultado do Teste Antigênico'].replace({'Negativo': 0, 'positivo': 1}, inplace= True)
+df.fillna(-1, inplace= True)
+df = df.astype(int)
+
+X = df.drop(columns= ['Resultado do Teste Antigênico'])
+y = df['Resultado do Teste Antigênico']
+
+print(y.unique())
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 14, stratify= y)
+
+underSampling = RandomUnderSampler(random_state= 14)
+X_train, y_train = underSampling.fit_resample(X_train, y_train)
+
+print(df.info())
+print(df.sample(10))
