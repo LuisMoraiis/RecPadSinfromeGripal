e611296086510b47f57d2cb22652f4659a9fc8d6
Luis Morais <fnluismoraiis.750@gmail.com>
2025-11-19 13:35:10 -0300

    teste

--- models/correlacaoPearson.py
+++ models/correlacaoPearson.py
@@ -1,14 +1,9 @@
 import pandas as pd
 
 import analysis.pre_processamento as prep
-import models.knn as knn
-import models.naive_bayes as nb
-import models.randomForest as rf
-import models.SVM as svm
-import models.xgboost as xgb
 
 y_true = prep.y_test.reset_index(drop=True)
-
+"""
 dic_pred_models = {
     "svm": pd.Series(svm.y_pred).reset_index(drop=True),
     "rf": pd.Series(rf.y_pred).reset_index(drop=True),
@@ -16,7 +11,7 @@ dic_pred_models = {
     "XGBoost": pd.Series(xgb.y_pred).reset_index(drop=True),
     "naive_bayes": pd.Series(nb.y_pred).reset_index(drop=True)
 }
-
+"""
 def calc_erro(dic_pred_models, y_true):
     erros = {}
     for nome, pred in dic_pred_models.items():
