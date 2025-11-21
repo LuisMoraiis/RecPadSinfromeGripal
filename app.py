5bf1f48aba4cb5eccc860123134795ba63e2ceff
Luis Morais <fnluismoraiis.750@gmail.com>
2025-11-21 11:15:51 -0300

    Adição do botão de download para as imagens

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+from io import BytesIO
+
 import streamlit as st
-import utils.helpersPlot as hp
 
 import models.votingClassifier as vc
+import utils.helpersPlot as hp
+
+
+def button_download(fig):
+    buffer = BytesIO()
+    fig.saving(buffer, format= "png")
+    return buffer.seek(0)
+
 
 st.title("Compilado de informações pro artigo")
 
 st.subheader("Correlação de Pearson")
 
-st.pyplot(hp.exibe_correlacao(vc.result_corr))
+fig = hp.exibe_correlacao(vc.result_corr)
+st.pyplot(fig)
+st.download_button(
+    label= "📥 Baixar gráfico",
+    data= button_download(fig),
+    file_name= "Correlação_Pearson.png",
+    mime= "image/png"
+)
